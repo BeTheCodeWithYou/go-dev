df9ff3f2c69b74b5d6c4401657dbde568e204480
Neeraj Sidhaye <bethecodewithyou@gmail.com>
2023-02-25 22:59:13 +0000

    adding test, pointer basic

--- deck-of-cards/deck_test.go
+++ deck-of-cards/deck_test.go
@@ -8,4 +8,8 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Errorf("expected 16 but found: %v", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Error("expected Ace of Spades but found ", d[0])
+	}
 }
--- dev-exercise/main.go
+++ dev-exercise/main.go
@@ -1,6 +1,36 @@
 package main
 
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+type contactInfo struct {
+	email    string
+	postcode string
+}
 
 func main() {
-	printState()
+	p := person{
+		firstName: "Neeraj",
+		lastName:  "sidhaye",
+		contactInfo: contactInfo{
+			email:    "test@gmail.com",
+			postcode: "m33",
+		},
+	}
+
+	p.updateName("Sid")
+	p.print()
+}
+
+func (p *person) updateName(name string) {
+	(*p).firstName = name
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
