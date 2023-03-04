603bf3886fe76a3c520ee15b9a3a10c7573cbb3f
Neeraj Sidhaye <bethecodewithyou@gmail.com>
2023-03-04 07:44:15 +0000

    quiz, struct examples

---
+++ dev-exercise/quiz/ite01/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	f, err := os.Open("../problems.csv")
+	if err != nil {
+		fmt.Printf("Error opening file %v", err)
+	}
+	defer f.Close()
+
+	r := csv.NewReader(f)
+	s, err := r.ReadAll()
+	if err != nil {
+		fmt.Printf("error reading file data %v", err)
+	}
+	// fmt.Println(s)
+	// q := s[1]
+	// fmt.Println(q)
+	// fmt.Println(q[0])
+	// fmt.Print(q[1])
+	var ua int
+	var crt int
+	var wng int
+	for _, q := range s {
+		fmt.Printf("What is %v ? ", q[0])
+		fmt.Scanln(&ua)
+		if err != nil {
+			fmt.Printf("Invalid input %v", err)
+		}
+		a, err := strconv.Atoi(q[1])
+		if err!=nil {
+			fmt.Println("Error converting string to int ", err)
+		}
+		if ua == a {
+			fmt.Println("Correct Answer!")
+			crt++
+		} else {
+			fmt.Println("Wrong!")
+			wng++
+		}
+
+	}
+	fmt.Printf("Correct %v \n", crt)
+	fmt.Printf("Wrong  %v ", wng)
+
+}
