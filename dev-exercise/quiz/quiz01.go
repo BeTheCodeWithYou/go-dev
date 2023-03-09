32be02279cbc3783671c287865241eea0ebfc5a9
Neeraj Sidhaye <bethecodewithyou@gmail.com>
2023-03-09 06:49:09 +0000

    added flag for file name as input

--- dev-exercise/quiz/quiz01.go
+++ dev-exercise/quiz/quiz01.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"os"
 	"strconv"
 )
 
 func main() {
-	f, err := os.Open("problems.csv")
+
+	var fn = flag.String("filename", "problems.csv", "file name containing questions")
+	flag.Parse()
+	fmt.Println(*fn)
+	f, err := os.Open(*fn)
 	if err != nil {
 		fmt.Printf("Error opening file %v", err)
 	}
@@ -19,14 +24,7 @@ func main() {
 	if err != nil {
 		fmt.Printf("error reading file data %v", err)
 	}
-	// fmt.Println(s)
-	// q := s[1]
-	// fmt.Println(q)
-	// fmt.Println(q[0])
-	// fmt.Print(q[1])
-	var ua int
-	var crt int
-	var wng int
+	var ua, crt, wrng int
 	for _, q := range s {
 		fmt.Printf("What is %v ? ", q[0])
 		fmt.Scanln(&ua)
@@ -34,19 +32,17 @@ func main() {
 			fmt.Printf("Invalid input %v", err)
 		}
 		a, err := strconv.Atoi(q[1])
-		if err!=nil {
+		if err != nil {
 			fmt.Println("Error converting string to int ", err)
 		}
 		if ua == a {
-			fmt.Println("Correct Answer!")
 			crt++
 		} else {
-			fmt.Println("Wrong!")
-			wng++
+			wrng++
 		}
 
 	}
 	fmt.Printf("Total Questions: %v \n", len(s))
 	fmt.Printf("Correct: %v \n", crt)
-	fmt.Printf("Wrong:  %v ", wng)
+	fmt.Printf("Wrong:  %v ", wrng)
 }
