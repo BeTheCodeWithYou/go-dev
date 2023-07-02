e8568c4436c1642995ad03aa8c3bc305bf384ef5
Neeraj Sidhaye <bethecodewithyou@gmail.com>
2023-07-02 22:09:51 +0100

    updated logger for taking io.writer

--- pocket-projects/pocketlog/logger.go
+++ pocket-projects/pocketlog/logger.go
@@ -1,55 +1,83 @@
 package pocketlog
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 // Logger for logging functionality
 type Logger struct {
 	threshold Level
+	output    io.Writer
+}
+
+// New returns a logger. Ready to log at required thereshold level
+func New(threshold Level, output io.Writer) *Logger {
+	return &Logger{
+		threshold: threshold,
+		output:    output,
+	}
 }
 
 func (l *Logger) DebugF(format string, args ...any) {
 
+	if l.output == nil {
+		l.output = os.Stdout
+	}
+
 	if l.threshold > LevelDebug {
 		return
 	}
+	l.logf(format, args...)
 
-	fmt.Printf(format+"\n", args...)
 }
 
 func (l *Logger) InfoF(format string, args ...any) {
+
+	if l.output == nil {
+		l.output = os.Stdout
+	}
 	if l.threshold > LevelInfo {
 		return
 	}
-
-	fmt.Printf(format+"\n ", args...)
-
+	l.logf(format, args...)
 }
 
 func (l *Logger) WarnF(format string, args ...any) {
+
+	if l.output == nil {
+		l.output = os.Stdout
+	}
 	if l.threshold > LevelWarn {
 		return
 	}
 
-	fmt.Printf(format+"\n", args...)
+	l.logf(format, args...)
 }
 
 func (l *Logger) ErrorF(format string, args ...any) {
+	if l.output == nil {
+		l.output = os.Stdout
+	}
 	if l.threshold > LevelError {
 		return
 	}
-
-	fmt.Printf(format+"\n", args...)
+	l.logf(format, args...)
 }
 
 func (l *Logger) FatalF(format string, args ...any) {
+	if l.output == nil {
+		l.output = os.Stdout
+	}
 	if l.threshold > LevelFatal {
 		return
 	}
 
-	fmt.Printf(format+"\n", args...)
+	l.logf(format, args...)
 }
 
-// New returns a logger. Ready to log at required thereshold level
-func New(threshold Level) *Logger {
-	return &Logger{threshold: threshold}
+// logf prints the message to the output and does formatting of the message.
+func (l *Logger) logf(format string, args ...any) {
+	_, _ = fmt.Fprintf(l.output, format+"\n", args...)
 }
--- pocket-projects/pocketlog/logger_test.go
+++ pocket-projects/pocketlog/logger_test.go
@@ -1,9 +1,11 @@
 package pocketlog_test
 
-import "pocket-projects/pocketlog"
+import (
+	"pocket-projects/pocketlog"
+)
 
 func ExampleLogger_DebugF() {
-	debugLogger := pocketlog.New(pocketlog.LevelDebug)
+	debugLogger := pocketlog.New(pocketlog.LevelDebug, nil)
 	debugLogger.DebugF("Hello, %s", "World")
 	// Output: Hello, World
 }
