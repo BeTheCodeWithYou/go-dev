6beafb9d8154a8819354c152656ebd90b65a4da5
Neeraj Sidhaye <bethecodewithyou@gmail.com>
2023-07-21 23:21:06 +0100

    defining bookworm construct and tidy up

---
+++ pocket-projects/bookworms/bookworm.go
@@ -0,0 +1,22 @@
+package main
+
+import "os"
+
+type Bookworm struct {
+	Name  string `json:"name"`
+	Books []Book `json:books"`
+}
+
+type Book struct {
+	Author string `json:"author"`
+	Title  string `json:"title"`
+}
+
+func loadBookworms(filePath string) ([]Bookworm, error) {
+
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, nil
+	}
+	defer f.Close()
+}
