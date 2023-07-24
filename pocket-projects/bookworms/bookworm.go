b430d5193ac11a965bf0291c11604bb0665736c8
Neeraj Sidhaye <bethecodewithyou@gmail.com>
2023-07-24 23:20:37 +0100

    decoding bookworm json

--- pocket-projects/bookworms/bookworm.go
+++ pocket-projects/bookworms/bookworm.go
@@ -1,15 +1,18 @@
 package main
 
-import "os"
-
-type Bookworm struct {
-	Name  string `json:"name"`
-	Books []Book `json:books"`
-}
+import (
+	"encoding/json"
+	"os"
+)
 
 type Book struct {
 	Author string `json:"author"`
 	Title  string `json:"title"`
+}
+
+type Bookworm struct {
+	Name  string `json:"name"`
+	Books []Book `json:books"`
 }
 
 func loadBookworms(filePath string) ([]Bookworm, error) {
@@ -19,4 +22,13 @@ func loadBookworms(filePath string) ([]Bookworm, error) {
 		return nil, nil
 	}
 	defer f.Close()
+
+	var bookworms []Bookworm
+
+	err = json.NewDecoder(f).Decode(&bookworms)
+	if err != nil {
+		return nil, err
+	}
+
+	return bookworms, nil
 }
--- pocket-projects/bookworms/main.go
+++ pocket-projects/bookworms/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main(){
-	
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	b, err := loadBookworms("testdata/bookworms.json")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "unable to load bookworms: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(b)
 }
