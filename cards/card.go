81d1257eb8024d9209e07a567d86a967242f53af
Neeraj Sidhaye <bethecodewithyou@gmail.com>
2023-09-25 21:12:38 +0100

    added function to read and write cards from/to file, shuffle card

--- cards/card.go
+++ cards/card.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"math/rand"
+	"os"
 	"strings"
+	"time"
 )
 
 type deck []string
@@ -39,3 +43,28 @@ func (d deck) toString() string {
 
 }
 
+func (d deck) saveToFile(s string) error {
+	return os.WriteFile(s, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(f string) deck {
+	bs, err := os.ReadFile(f)
+	if err != nil {
+		log.Fatal("Error in reading from file {}", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
+}
--- cards/main.go
+++ cards/main.go
@@ -1,20 +1,7 @@
 package main
 
-import (
-	"log"
-	"os"
-)
-
 func main() {
 	cards := newDeck()
-	//cards.print()
-
-	err := os.WriteFile("testdata/cards", []byte(cards.toString()), 0666)
-	if err != nil {
-		log.Fatal("error writing to file {}", err)
-	}
-
-	//	d1, d2 := cards.hand(3)
-	//	d1.print()
-	//	d2.print()
+	cards.shuffle()
+	cards.print()
 }
